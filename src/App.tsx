4c7b1caddfa93bcd2ae2a142097ea3531a4dde3f
jack-fajardo <jackfajardotech@gmail.com>
2023-06-19 04:00:15 +0800

    adds flags to onFocus approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import { Input, Text } from '@chakra-ui/react'
+import { CURRENCIES } from '../src/constants.js' ;
+
+const flag = 3;
 
 const validate = (num: string) => {
   return /^[.,0-9]+$/.test(num);
 }
 
-const formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
-
 export const App: React.FC = () => {
+  const [locale] = useState<string>(CURRENCIES[flag].locale);
+  const [currencyCode] = useState<string>(CURRENCIES[flag].currency);
   const [isInvalid, setIsInvalid] = useState<boolean>(false);
   const [number, setNumber] = useState<string>("");
   const [currency, setCurrency] = useState<string>("");
-
 
   const checkIfEmpty = (n: string) => {
     if (!n) {
@@ -22,10 +24,6 @@ export const App: React.FC = () => {
     return false;
   }
 
-  const onFocus = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrency(number);
-  };
-
   const onBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsInvalid(false);
 
@@ -34,7 +32,10 @@ export const App: React.FC = () => {
     } = event;
     if (checkIfEmpty(value)) {return;}
 
-    const newValue = formatter.format(value);
+    const newValue = new Intl.NumberFormat(
+      locale, 
+      { style: 'currency', currency: currencyCode }
+    ).format(value);
     
     if (newValue.toString().includes("NaN")) {
       setIsInvalid(true);
@@ -61,22 +62,26 @@ export const App: React.FC = () => {
   };
 
   return (
-    <div style={{ padding: '130px' }}>
-      <Input 
-        value={currency}
-        onChange={(e) => onChange(e)}
-        onFocus={(e) => onFocus(e)}
-        onBlur={(e) => onBlur(e)}
-        placeholder="Enter an amount"
-        isInvalid={isInvalid}
-        errorBorderColor='crimson'
-      />
-      <br/><br/>
+    <div>
+      <div style={{ display: 'flex', padding: '130px' }}>
+        <Input 
+          value={currency}
+          onChange={(e) => onChange(e)}
+          onFocus={() => setCurrency(number)}
+          onBlur={(e) => onBlur(e)}
+          placeholder="Enter an amount"
+          isInvalid={isInvalid}
+          errorBorderColor='crimson'
+        />
+      </div>
+      
+      <div style={{ marginLeft: '130px' }}>
       {
         currency && (
           <Text fontSize="3xl">{currency}</Text>
         )
       }
+      </div>
     </div>
   )
 }
---
+++ src/constants.js
@@ -0,0 +1,19 @@
+
+export const CURRENCIES = [
+    {
+      locale: 'en-US', 
+      currency: 'USD',
+    },
+    {
+      locale: 'de-DE', 
+      currency: 'EUR',
+    },
+    {
+      locale: 'ja-JP', 
+      currency: 'JPY',
+    },
+    {
+      locale: 'zh-CN', 
+      currency: 'CNY',
+    },
+];
