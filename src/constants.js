d36ae4b237c0ce091a5d43c208f1fd02c47122ae
jack-fajardo <jackfajardotech@gmail.com>
2023-06-19 11:57:08 +0800

    adds flags to onFocus approach

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,32 @@
-
 export const CURRENCIES = [
-    {
-      locale: 'en-US', 
-      currency: 'USD',
-    },
-    {
-      locale: 'de-DE', 
-      currency: 'EUR',
-    },
-    {
-      locale: 'ja-JP', 
-      currency: 'JPY',
-    },
-    {
-      locale: 'zh-CN', 
-      currency: 'CNY',
-    },
+  { locale: 'en-US', currencyCode: 'USD' },
+  { locale: 'en-GB', currencyCode: 'GBP' },
+  { locale: 'fr-FR', currencyCode: 'EUR' },
+  { locale: 'de-DE', currencyCode: 'EUR' },
+  { locale: 'ja-JP', currencyCode: 'JPY' },
+  { locale: 'zh-CN', currencyCode: 'CNY' },
+  { locale: 'ar-SA', currencyCode: 'SAR' },
+  { locale: 'es-ES', currencyCode: 'EUR' },
+  { locale: 'pt-BR', currencyCode: 'BRL' },
+  { locale: 'ru-RU', currencyCode: 'RUB' },
+  { locale: 'af-ZA', currencyCode: 'ZAR' },
+  { locale: 'sq-AL', currencyCode: 'ALL' },
+  { locale: 'am-ET', currencyCode: 'ETB' },
+  { locale: 'hy-AM', currencyCode: 'AMD' },
+  { locale: 'az-AZ', currencyCode: 'AZN' },
+  { locale: 'eu-ES', currencyCode: 'EUR' },
+  { locale: 'be-BY', currencyCode: 'BYN' },
+  { locale: 'bn-BD', currencyCode: 'BDT' },
+  { locale: 'bs-BA', currencyCode: 'BAM' },
+  { locale: 'bg-BG', currencyCode: 'BGN' },
+  { locale: 'ca-ES', currencyCode: 'EUR' }, // Catalan
+  { locale: 'hr-HR', currencyCode: 'HRK' }, // Croatian
+  { locale: 'cs-CZ', currencyCode: 'CZK' }, // Czech
+  { locale: 'da-DK', currencyCode: 'DKK' }, // Danish
+  { locale: 'nl-NL', currencyCode: 'EUR' }, // Dutch
+  { locale: 'et-EE', currencyCode: 'EUR' }, // Estonian
+  { locale: 'fi-FI', currencyCode: 'EUR' }, // Finnish
+  { locale: 'gl-ES', currencyCode: 'EUR' }, // Galician
+  { locale: 'ka-GE', currencyCode: 'GEL' }, // Georgian
+  { locale: 'el-GR', currencyCode: 'EUR' }, // Greek
 ];
